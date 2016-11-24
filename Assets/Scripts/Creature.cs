bf18015f0557e8dd78f4133b04605b4284e3adbb
Ryo Meguro <ryomeguro@gmail.com>
2016-11-24 20:55:32 +0900

    editCreature.cs

--- Assets/Scripts/Creature.cs
+++ Assets/Scripts/Creature.cs
@@ -3,18 +3,30 @@ using System.Collections;
 
 public class Creature : MonoBehaviour {
 
+	enum Direction{UP,DOWN,LEFT,RIGHT};
+
 	[SerializeField]
-	int hp;
+	int maxHp;
 	[SerializeField]
 	int gold;
 
+	int hp;
+
 	// Use this for initialization
 	void Start () {
-	
+		hp = maxHp;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void damaged(){
+
+	}
+
+	void move(){
+
 	}
 }
