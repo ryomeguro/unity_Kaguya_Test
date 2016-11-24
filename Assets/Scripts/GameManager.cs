27d8000186a5c7f9fbf2964b2e015f551a5261c1
Ryo Meguro <ryomeguro@gmail.com>
2016-11-24 13:41:43 +0900

    色々追加
    スプライトとスクリプトを追加

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Cube : MonoBehaviour {
+public class GameManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		int i = 2;
-		int j = 3;
+	
 	}
 	
 	// Update is called once per frame
