df8193923dd4cb864f7186690c829f774f042e7b
KSKsurigoma <u775032i@ecs.osaka-u.ac.jp>
2016-11-24 21:38:48 +0900

    ieeeeeeeeeei！！！消しました(泣)

--- Assets/Scripts/Creature.cs
+++ Assets/Scripts/Creature.cs
@@ -14,20 +14,10 @@ public class Creature : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-<<<<<<< HEAD
-<<<<<<< HEAD:Assets/Cube.cs
-		int i = 2;
-		int j = 3000000;
-=======
-	
->>>>>>> origin/master:Assets/Scripts/Creature.cs
-=======
 		hp = maxHp;
->>>>>>> origin/master
 	}
 	
 	// Update is called once per frame
-	//ieeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeei!!!!!!!!!!!
 	void Update () {
 	
 	}
