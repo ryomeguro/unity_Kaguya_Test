662f68d631450e9e8c4e70d76cfc7c86efeaacbd
Yasuhiro123 <yasuhiro@mukaiyasuhiro-no-MacBook-Air.local>
2016-11-25 10:52:32 +0900

    item
    mukai made item class.

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections;
+
+public class Item : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	void effects ()
+	{
+	
+	}
+}
+
